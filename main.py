f272c879af06fb0196318f94122b0cb2568a458b
sondre <z5bewil7@anonaddy.me>
2024-09-11 12:18:18 +0200

    Implement /wordle command

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from dotenv import load_dotenv
 from os import getenv
 import re
+import wordle
 
 load_dotenv()
 discord_token = getenv("DISCORD_TOKEN")
@@ -11,17 +12,31 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+tree = discord.app_commands.CommandTree(client)
+print("Playing Wordle...")
+daily_stats = wordle.play_wordle()
+print(daily_stats)
+
+@tree.command(
+    name="wordle",
+    description="Check Today's Penis Gambit",
+    guild=discord.Object(id=547041619656966185)
+)
+async def wordle(interaction):
+    await interaction.response.send_message(daily_stats)
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
+    await tree.sync(guild=discord.Object(id=547041619656966185))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if re.search(r'd+i+c+k+', message.content, re.IGNORECASE):
-        await message.channel.send('butt')
-    if re.search(r'h+o+w+', message.content, re.IGNORECASE):
-        await message.channel.send('idk lol')
+    if re.search(r"d+i+c+k+", message.content, re.IGNORECASE):
+        await message.channel.send("butt")
+    if re.search(r"h+o+w+", message.content, re.IGNORECASE):
+        await message.channel.send("idk lol")
 
 client.run(discord_token)
--- wordle.py
+++ wordle.py
@@ -5,41 +5,50 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-url = "https://www.nytimes.com/games/wordle/index.html"
+def play_wordle():
+    url = "https://www.nytimes.com/games/wordle/index.html"
 
-driver = webdriver.Firefox()
-driver.get(url)
-title = driver.title
-wait = WebDriverWait(driver, timeout=10)
-wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "fides-reject-all-button")))
-cookie_button = driver.find_element(by=By.CLASS_NAME, value="fides-reject-all-button")
-print(cookie_button)
-play_button = driver.find_element(by=By.CLASS_NAME, value="Welcome-module_button__ZG0Zh:nth-child(3)")
-print(play_button)
+    driver = webdriver.Firefox()
+    driver.get(url)
 
-cookie_button.click()
-driver.implicitly_wait(0.5)
-play_button.click()
+    wait = WebDriverWait(driver, timeout=10)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "fides-reject-all-button")))
+    cookie_button = driver.find_element(by=By.CLASS_NAME, value="fides-reject-all-button")
+    play_button = driver.find_element(by=By.CLASS_NAME, value="Welcome-module_button__ZG0Zh:nth-child(3)")
+    cookie_button.click()
+    driver.implicitly_wait(0.5)
+    play_button.click()
 
-wait = WebDriverWait(driver, timeout=10)
-wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "Modal-module_closeIcon__TcEKb")))
-close_button = driver.find_element(by=By.CLASS_NAME, value="Modal-module_closeIcon__TcEKb")
-print(close_button)
-close_button.click()
+    wait = WebDriverWait(driver, timeout=10)
+    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "Modal-module_closeIcon__TcEKb")))
+    close_button = driver.find_element(by=By.CLASS_NAME, value="Modal-module_closeIcon__TcEKb")
+    close_button.click()
 
-for x in "penis↵":
-    button = driver.find_element(By.CSS_SELECTOR, f'[data-key="{x}"]').click()
+    for x in "penis↵":
+        button = driver.find_element(By.CSS_SELECTOR, f'[data-key="{x}"]').click()
 
-sleep(2)
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-penis_dict = dict()
-for x in soup.find_all("div"):
-    if x.get("aria-live") == "polite":
-        val = x.get("data-state")
-        letter = x.get("aria-label")
-        print(f"{letter[12]}: {val}")
-        penis_dict[letter[12]] = val
-print(penis_dict)
-driver.quit()
-return penis_dict
+    sleep(2)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    penis_dict = dict()
+    for x in soup.find_all("div"):
+        if x.get("aria-live") == "polite":
+            val = x.get("data-state")
+            letter = x.get("aria-label")
+            penis_dict[letter[12]] = val
+    driver.quit()
+
+    msg = ""
+    for val in penis_dict.values():
+        match val:
+            case "absent":
+                msg += ":black_large_square:"
+            case "present":
+                msg += ":yellow_square:"
+            case "correct":
+                msg += ":green_square:"
+    print(penis_dict)
+    print(msg)
+    return msg
+
