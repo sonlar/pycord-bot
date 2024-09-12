247a224e79d3fe8da6c1f91fe288010d9ea83811
sondre <z5bewil7@anonaddy.me>
2024-09-12 14:38:28 +0200

    Add script that takes a screenshot of the free games section

---
+++ epic.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Firefox()
+driver.get("https://store.epicgames.com/en-US/free-games")
+driver.implicitly_wait(5)
+
+games = driver.find_element(By.CLASS_NAME, "css-2u323")
+driver.execute_script("arguments[0].scrollIntoView(true);", games)
+driver.implicitly_wait(5)
+
+driver.find_element(By.ID, "onetrust-reject-all-handler").click()
+driver.implicitly_wait(5)
+games.screenshot("./pics/test.png")
+
+driver.quit()
