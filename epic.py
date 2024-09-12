44bc63ef77776d57fa0a18dc5b64b84a91139ac5
sondre <z5bewil7@anonaddy.me>
2024-09-12 23:20:14 +0200

    Add functionality to send screenshot of current and upcoming free games with a timestamp for when they go away

--- epic.py
+++ epic.py
@@ -1,32 +1,37 @@
-import check
 import datetime
 import json
 import re
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def take_screenshot():
     driver = webdriver.Firefox()
     driver.get("https://store.epicgames.com/en-US/free-games")
     driver.implicitly_wait(5)
+    
+    wait = WebDriverWait(driver, timeout=10)
+    wait.until(EC.visibility_of_element_located((By.ID, "onetrust-reject-all-handler")))
+    driver.find_element(By.ID, "onetrust-reject-all-handler").click()
+
 
     games = driver.find_element(By.CLASS_NAME, "css-2u323")
     driver.execute_script("arguments[0].scrollIntoView(true);", games)
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(5) 
 
-    driver.find_element(By.ID, "onetrust-reject-all-handler").click()
     driver.implicitly_wait(5)
     games.screenshot("./pics/test.png")
     
-    driver.implicitly_wait(2)
+    driver.implicitly_wait(5)
     date = driver.find_element(By.TAG_NAME, "time").get_attribute("datetime")
 
     driver.quit()
     return date
 
 def has_expired():
-    check
+    print("in has_expired")
     with open("data.json", "r") as f:
         data = json.load(f)
     current_date = datetime.datetime.now(datetime.timezone.utc)
@@ -34,15 +39,23 @@ def has_expired():
     epic_timestamp = data["epic"]["timestamp"]
 
     if epic_timestamp < current_timestamp:
-        data["epic"]["timestamp"] = take_screenshot()
+        print("in epic < current")
+        try:
+            data["epic"]["timestamp"] = take_screenshot()
+            split_timestamp = [date for date in re.split("-|T|:", data["epic"]["timestamp"])]
+            split_timestamp = split_timestamp[:5]
+            split_timestamp = [int(date) for date in split_timestamp]
+            split_timestamp[3] += 2
 
-        split_timestamp = [date for date in re.split("-|T|:", data["epic"]["timestamp"])]
-        split_timestamp = split_timestamp[:5]
-        split_timestamp = [int(date) for date in split_timestamp]
+            epoch_timestamp = datetime.datetime(split_timestamp[0], split_timestamp[1], split_timestamp[2], split_timestamp[3], split_timestamp[4]).timestamp()
+            data["epic"]["discord_timestamp"] = int(epoch_timestamp)
 
-        epoch_timestamp = datetime.datetime(split_timestamp[0], split_timestamp[1], split_timestamp[2], split_timestamp[3], split_timestamp[4]).timestamp()
-        data["epic"]["discord_timestamp"] = epoch_timestamp
-
-        with open("data.json", "w") as f:
-            json.dump(data, f, indent=4)
+            with open("data.json", "w") as f:
+                json.dump(data, f, indent=4)
+            return True, int(epoch_timestamp)
+        except TimeoutException:
+            print("timed out, probably anti-bot")
+ 
+    print("in epic > current")
+    return False, 0
 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import check
+import concurrent.futures
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
+import epic
+import json
 from os import getenv
 import re
 import last_wordle
@@ -7,6 +12,7 @@ import last_wordle
 load_dotenv()
 discord_token = getenv("DISCORD_TOKEN")
 guild_id = getenv("GUILD_ID")
+announcement_channel = getenv("ANNOUNCEMENT_CHANNEL")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,6 +20,8 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 tree = discord.app_commands.CommandTree(client)
+
+check
 
 def wordle_check():
     score = last_wordle.check_time()
@@ -32,6 +40,9 @@ async def on_ready():
     print(f"We have logged in as {client.user}")
     await tree.sync(guild=discord.Object(id=guild_id))
 
+    announcement.start()
+    print("starting announcement")
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -40,5 +51,15 @@ async def on_message(message):
         await message.channel.send("butt")
     if re.search(r"h+o+w+", message.content, re.IGNORECASE):
         await message.channel.send("idk lol")
+
+@tasks.loop(minutes=1)
+async def announcement():
+    guild = client.get_guild(int(guild_id))
+    channel_id = guild.get_channel(int(announcement_channel))
+    print("in announcement")
+    bool_val, epoch = epic.has_expired()
+    if bool_val:
+        print("bool was True")
+        await channel_id.send(content=f"CLAIM BEFORE: <t:{epoch}:f>",file=discord.File("./pics/test.png"))
 
 client.run(discord_token)
