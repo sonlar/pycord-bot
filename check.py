f7a64f6ffb3b67a6ec626b3c6f8dfa540e47de7b
sondre <z5bewil7@anonaddy.me>
2024-09-12 17:12:06 +0200

    Update check.py

--- check.py
+++ check.py
@@ -1,5 +1,7 @@
 import json
 import os
+keys = ["epic"]
+sub_keys = [["timestamp"]]
 
 # Check if Folder "pics" exists
 if not os.path.isdir("pics"):
@@ -7,5 +9,20 @@ if not os.path.isdir("pics"):
 
 # Check if data.json exists
 if not os.path.isfile("data.json"):
-    with open("data.json"):
-        pass
+    with open("data.json", "w") as f:
+        json.dump({}, f)
+
+# Check if epic key exists
+with open("data.json", "r") as f:
+    data = json.load(f)
+for pos,key in enumerate(keys):
+    if not key in data:
+        data[key] = {}
+    for sub_key in sub_keys[pos]:
+        if not sub_key in data[key]:
+            data[key][sub_key] = ""
+    
+with open("data.json", "w") as f:
+    json.dump(data, f, indent=4)
+
+
